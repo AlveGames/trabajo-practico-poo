f692197c6b9afd28a9626359f2a21246ffdbca2b
alvegames <renjiYT1978@gmail.com>
2026-05-14 16:49:26 -0500

    primeros bloques

--- block_3/Block_3.py
+++ block_3/Block_3.py
@@ -1,3 +1,6 @@
+
+
+#1. Con a=20, b=4 imprime todos los operadores aritméticos y sus resultados.
 a = 20
 b = 4
 
@@ -9,6 +12,11 @@ print(f"Integer division: {a // b}")
 print(f"Modulo: {a % b}")
 print(f"Power: {a ** b}")
 
+
+print("--------------------------------------------------------------")
+# 2. Crea dos listas idénticas y demuestra que == es True pero is es False.
+#  ✓ a=[1,2]; b=[1,2]; a==b→True; a is b→False
+
 list1 = [1, 2, 3]
 list2 = [1, 2, 3]
 list3 = list1
@@ -17,6 +25,10 @@ print(f"list1 == list2: {list1 == list2}")
 print(f"list1 is list2: {list1 is list2}")
 print(f"list1 is list3: {list1 is list3}")
 
+
+print("--------------------------------------------------------------")
+# 3. Evalúa: x = 2 + 1 * 2 % 2 + (2**1)//2 → explica el orden de evaluación.
+#  ✓  Resultado: 3 (** → % → // → +)
 """
 x = 2 + 1 * 2 % 2 + (2**1)//2
 1. (2**1) = 2
